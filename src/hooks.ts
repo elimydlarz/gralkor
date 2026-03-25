ed1a56d591d6fec4616f7a3d06015ff1d8a54786
Eli Mydlarz <eli.mydlarz@gmail.com>
2026-03-25 14:15:10 +0700

    auto(0c503bd2): Injection is returning nothing for stuff that should be in the graph. St
    File: src/hooks.ts
    Session: 0c503bd2-bd45-4380-be65-d53823e7da3b

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,6 +1,6 @@
 import type { GraphitiClient, Fact, EpisodeMessage, EpisodeBlock } from "./client.js";
 import type { GralkorConfig } from "./config.js";
-import { resolveGroupId, BOOTING_MSG, type ReadyGate } from "./config.js";
+import { resolveGroupId, type ReadyGate } from "./config.js";
 import { formatFact } from "./tools.js";
 
 /**
