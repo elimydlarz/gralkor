e05877faff5c626ba2672286526aaaa70b72cb0d
Eli Mydlarz <eli.mydlarz@gmail.com>
2026-03-29 17:17:11 +0700

    auto(2c42ab04): Propose simplifications
    File: src/hooks.ts
    Session: 2c42ab04-c04a-4117-baeb-e6a91aebdd7c

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,7 +1,7 @@
 import type { GraphitiClient, EpisodeMessage, EpisodeBlock } from "./client.js";
 import type { GralkorConfig } from "./config.js";
 import { type ReadyGate } from "./config.js";
-import { formatFact } from "./tools.js";
+import { formatFact, INTERPRETATION_INSTRUCTION } from "./tools.js";
 
 /**
  * A content block inside a message entry.
