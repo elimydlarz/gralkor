2592fd25b460770776fddceaf4eca1d2f27946e9
Eli Mydlarz <eli.mydlarz@gmail.com>
2026-04-02 17:42:31 +0700

    auto(494e27d3): plugin install problem on agent:
    File: src/cli/commands/install.test.ts
    Session: 494e27d3-57c3-43d2-96c6-314863857a6f

--- src/cli/commands/install.test.ts
+++ src/cli/commands/install.test.ts
@@ -19,7 +19,6 @@ beforeEach(() => {
   mocked.enablePlugin.mockResolvedValue(undefined);
   mocked.setConfig.mockResolvedValue(undefined);
   mocked.removePluginDir.mockResolvedValue(undefined);
-  mocked.unsetConfig.mockResolvedValue(undefined);
 });
 
 describe("install", () => {
