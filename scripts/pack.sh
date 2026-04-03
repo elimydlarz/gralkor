c35e165f8b799f1609e4aea7995785cde954c04f
Eli Mydlarz <eli.mydlarz@gmail.com>
2026-04-03 19:04:07 +0700

    auto(2939a6c7): Failure on load - did we change anything to do with this recently?
    File: scripts/pack.sh
    Session: 2939a6c7-a0fb-4ce3-bfca-a0b62e0be162

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -5,25 +5,7 @@ pnpm run --silent build
 
 version=$(node -p "require('./resources/memory/package.json').version")
 
-FALKORDBLITE_VERSION="v0.9.0"
-
-echo "Building falkordblite wheel for linux/arm64..."
-rm -rf server/wheels
-mkdir -p server/wheels
-docker run --rm --platform linux/arm64 \
-  -v "$(pwd)/server/wheels:/out" \
-  python:3.13-bookworm \
-  bash -c "
-    set -e
-    apt-get update -qq && apt-get install -y -qq build-essential git > /dev/null
-    git clone --depth 1 --branch ${FALKORDBLITE_VERSION} https://github.com/FalkorDB/falkordblite.git /tmp/fdb
-    cd /tmp/fdb
-    pip install --quiet wheel setuptools
-    python setup.py bdist_wheel
-    python -m wheel tags --remove --python-tag py3 --abi-tag none --platform-tag manylinux_2_36_aarch64 dist/*.whl
-    cp dist/*.whl /out/
-  "
-ls server/wheels/
+bash scripts/build-arm64-wheel.sh
 
 cp resources/memory/package.json package.json
 cp resources/memory/openclaw.plugin.json openclaw.plugin.json
