eeb71b434dd7778092c215a61fe7949704684723
Eli Mydlarz <eli.mydlarz@gmail.com>
2026-04-18 00:34:03 +0700

    auto(e31c50fb): We wrap Gralkor for TS, but now we also need to wrap it for Jido. Discus
    File: ex/mix.exs
    Session: e31c50fb-9682-4c65-a326-f030b0ca7136

---
+++ ex/mix.exs
@@ -0,0 +1,46 @@
+defmodule Gralkor.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :gralkor,
+      version: "0.1.0",
+      elixir: "~> 1.17",
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      releases: releases(),
+      aliases: aliases(),
+      test_coverage: [summary: [threshold: 0]]
+    ]
+  end
+
+  def application do
+    [
+      mod: {Gralkor.Application, []},
+      extra_applications: [:logger, :inets, :ssl]
+    ]
+  end
+
+  defp deps do
+    [
+      {:req, "~> 0.5"},
+      {:jason, "~> 1.4"}
+    ]
+  end
+
+  defp releases do
+    [
+      gralkor: [
+        include_executables_for: [:unix],
+        applications: [runtime_tools: :permanent]
+      ]
+    ]
+  end
+
+  defp aliases do
+    [
+      "test.unit": ["test --exclude integration"],
+      "test.integration": ["test --only integration"]
+    ]
+  end
+end
