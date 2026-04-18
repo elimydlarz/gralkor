c1d19c5e15c930f06f4c99cb6f1d510a0b7d9c83
Eli Mydlarz <eli.mydlarz@gmail.com>
2026-04-18 22:46:41 +0700

    auto(99079da0): How do I publish to Hex?
    File: ex/lib/mix/tasks/compile.gralkor_priv.ex
    Session: 99079da0-31fa-4feb-8cd4-8b2df9774c30

--- ex/lib/mix/tasks/compile.gralkor_priv.ex
+++ ex/lib/mix/tasks/compile.gralkor_priv.ex
@@ -13,6 +13,7 @@ defmodule Mix.Tasks.Compile.GralkorPriv do
 
     if File.dir?(src) do
       dest = Path.expand(@priv_dest, File.cwd!())
+      File.rm_rf!(dest)
       File.mkdir_p!(dest)
       copy_tree(src, dest)
       :ok
@@ -26,32 +27,16 @@ defmodule Mix.Tasks.Compile.GralkorPriv do
       src_path = Path.join(src, entry)
       dest_path = Path.join(dest, entry)
 
-      cond do
-        File.dir?(src_path) ->
-          File.mkdir_p!(dest_path)
-          copy_tree(src_path, dest_path)
-
-        stale?(src_path, dest_path) ->
-          File.cp!(src_path, dest_path)
-
-        true ->
-          :ok
+      if File.dir?(src_path) do
+        File.mkdir_p!(dest_path)
+        copy_tree(src_path, dest_path)
+      else
+        File.cp!(src_path, dest_path)
       end
     end
   end
 
   defp keep?(entry) do
     entry not in @skip_dirs and Path.extname(entry) not in @skip_ext
-  end
-
-  defp stale?(src_path, dest_path) do
-    case File.stat(dest_path) do
-      {:ok, %{mtime: dest_mtime}} ->
-        {:ok, %{mtime: src_mtime}} = File.stat(src_path)
-        dest_mtime < src_mtime
-
-      {:error, _} ->
-        true
-    end
   end
 end
